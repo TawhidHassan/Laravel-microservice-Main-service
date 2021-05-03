8305ef22849788a0b8f424859f6c181420f6673d
sifat hassan <sifat007.sh7@gmail.com>
2021-05-03 15:41:59 +0600

    event listent by admin app

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,31 +2,19 @@
 
 namespace App\Providers;
 
+use App\Jobs\TestJob;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
-
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
+   
     public function boot()
     {
-        //
+        \App::bindMethod(TestJob::class,'@handle',function($job){
+           return $job->hande();
+        });
     }
 }
