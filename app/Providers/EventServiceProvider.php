1c7ded31d1e4e07aedb8d104ff1824894db8d727
sifat hassan <sifat007.sh7@gmail.com>
2021-05-04 14:25:34 +0600

    listen the product event and take action in main app database

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
-use App\Jobs\TestJob;
+
+use App\Jobs\ProductCreated;
+use App\Jobs\ProductDeleted;
+use App\Jobs\ProductUpdated;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -13,8 +16,16 @@ class EventServiceProvider extends ServiceProvider
    
     public function boot()
     {
-        \App::bindMethod(TestJob::class,'@handle',function($job){
-           return $job->hande();
-        });
+        
+        \App::bindMethod(ProductCreated::class,'@handle',function($job){
+            return $job->hande();
+         });
+         \App::bindMethod(ProductUpdated::class,'@handle',function($job){
+            return $job->hande();
+         });
+         \App::bindMethod(ProductDeleted::class,'@handle',function($job){
+            return $job->hande();
+         });
+
     }
 }
